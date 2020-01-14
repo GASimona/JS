bfa1405ccec72c21ee749d47d7a3062ae3f4a59c
JavaScript <javascript@Admins-iMac-82.local>
2020-01-14 20:59:32 +0200

    spanzuratoarea visual

--- hangman/hang.js
+++ hangman/hang.js
@@ -7,13 +7,13 @@ var game =  {
     start: function() {
         this.word = this.randomWord();
     },
-    randomWord = function() {
+    randomWord : function() {
         let index = Math.floor(Math.random() * this.words.length);
         return this.words[index];
     },
     tryLetter: function(letter) {
         this.letters.push(letter);
-        if ((!this.word.split('')).includes(letter)) {
+        if (!this.word.split('').includes(letter)) {
             this.triesLeft--;
         }
     },
@@ -21,6 +21,50 @@ var game =  {
         return this.triesLeft == 0;
     },
     isWinner: function() {
-        
+        return this.word.split('')
+                        .every(letter => this.letters.includes(letter));
     }
 }
+
+function renderLetters() {
+    document.getElementById("letters").innerText = game.letters.join(' ');
+
+}
+
+function renderWord() {
+    let text = game.word.split('')
+                        .map
+                        ( 
+                            letter => game.letters.includes(letter) ? letter : '_'
+                        )
+                        .join(' ');
+    document.getElementById("word").innerText = text;
+}
+
+function renderDrawing() {
+    let parts = ['head', 'body', 'left_arm', 'right_arm', 'left_leg', 'right_leg'];
+    let visible = parts.slice(0, 6 - game.triesLeft);
+    let invisible =parts.slice(6 - game.triesLeft);
+    visible.forEach(name => document.getElementById(name).style.visible = 'visible' );
+
+}
+
+function renderView() {
+
+// game.word = 'APPALE';
+// game.letters = ['A', 'E', 'S'];
+// game.triesLeft = 4;
+
+    renderLetters();
+    renderWord();
+    renderDrawing();
+}
+
+window.addEventListener('keydown', function(event) {
+    if (game.isDead() || game.isWinner() ) return;
+    game.tryLetter(event.key);
+    renderView();
+} );
+
+game.start();
+renderView();
